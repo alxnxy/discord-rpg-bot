fee6dfa1900f2b68417acf1f8ecf03310796f861
Alexander Raventhorn <arrafiwahyuandreashakim@gmail.com>
2025-03-31 19:23:39 +0700

    Update rpg.py

--- cogs/rpg.py
+++ cogs/rpg.py
@@ -1,1 +1,16 @@
+import discord
+from discord.ext import commands
+from database import Database
 
+class RPGSystem(commands.Cog):
+    def __init__(self, bot, db):
+        self.bot = bot
+        self.db = db
+
+    @commands.command()
+    async def profile(self, ctx):
+        user = self.db.get_user(ctx.author.id)
+        embed = discord.Embed(title=f"{ctx.author}'s Profile")
+        embed.add_field(name="Level", value=user['level'])
+        embed.add_field(name="HP", value=f"{user['hp']}/{user['max_hp']}")
+        await ctx.send(embed=embed)
