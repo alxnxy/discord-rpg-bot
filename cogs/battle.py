24cd24061f41d3c6a50c315eb41c0ec53f4714f8
Alexander Raventhorn <arrafiwahyuandreashakim@gmail.com>
2025-03-31 19:25:28 +0700

    Update battle.py

--- cogs/battle.py
+++ cogs/battle.py
@@ -1,1 +1,9 @@
+class BattleSystem(commands.Cog):
+    def __init__(self, bot, db):
+        self.bot = bot
+        self.db = db
 
+    @commands.command()
+    async def hunt(self, ctx):
+        monster = random.choice(self.db.monsters)
+        await ctx.send(f"You encountered a {monster['name']}! Use `!attack` to fight!")
