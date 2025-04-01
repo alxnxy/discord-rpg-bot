6cf26feb9d6fdd754d978733436b97a72ebc7afa
Alexander Raventhorn <arrafiwahyuandreashakim@gmail.com>
2025-04-01 10:28:08 +0700

    Update guild.py

--- cogs/guild.py
+++ cogs/guild.py
@@ -1,1 +1,9 @@
+class GuildSystem(commands.Cog):
+    def __init__(self, bot, db):
+        self.bot = bot
+        self.db = db
 
+    @commands.command()
+    async def create_guild(self, ctx, name: str):
+        self.db.create_guild(ctx.author.id, name)
+        await ctx.send(f"Guild {name} berhasil dibuat!")
