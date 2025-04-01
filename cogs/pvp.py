96b3c14b9c9fd1624b5a6ae8f9cc3a47556a0ae7
Alexander Raventhorn <arrafiwahyuandreashakim@gmail.com>
2025-04-01 10:32:56 +0700

    Update pvp.py

--- cogs/pvp.py
+++ cogs/pvp.py
@@ -1,1 +1,8 @@
+class PVPSystem(commands.Cog):
+    def __init__(self, bot, db):
+        self.bot = bot
+        self.db = db
 
+    @commands.command()
+    async def duel(self, ctx, opponent: discord.Member):
+        await ctx.send(f"Duel request sent to {opponent.mention}!")
