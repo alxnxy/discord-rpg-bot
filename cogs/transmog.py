07940029172d770a3722b4b0dd6012ecca53255f
Alexander Raventhorn <arrafiwahyuandreashakim@gmail.com>
2025-04-01 10:36:15 +0700

    Update transmog.py

--- cogs/transmog.py
+++ cogs/transmog.py
@@ -1,1 +1,8 @@
+class TransmogSystem(commands.Cog):
+    def __init__(self, bot, db):
+        self.bot = bot
+        self.db = db
 
+    @commands.command()
+    async def transmog(self, ctx, item: str):
+        await ctx.send(f"Item {item} berhasil diubah tampilannya!")
