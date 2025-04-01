1df692319abf757d9f05961df2c6fa3c6d58be26
Alexander Raventhorn <arrafiwahyuandreashakim@gmail.com>
2025-04-01 10:42:29 +0700

    Update dynamic.py

--- cogs/dynamic.py
+++ cogs/dynamic.py
@@ -1,1 +1,8 @@
+class DynamicSystem(commands.Cog):
+    def __init__(self, bot, db):
+        self.bot = bot
+        self.db = db
 
+    @commands.command()
+    async def weather(self, ctx):
+        await ctx.send("Cuaca saat ini: Cerah")
