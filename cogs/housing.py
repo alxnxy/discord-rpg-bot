adc91b5eb97ab2846e6b8d698fe1079ac5e6f333
Alexander Raventhorn <arrafiwahyuandreashakim@gmail.com>
2025-04-01 10:35:04 +0700

    Update housing.py

--- cogs/housing.py
+++ cogs/housing.py
@@ -1,1 +1,8 @@
+class HousingSystem(commands.Cog):
+    def __init__(self, bot, db):
+        self.bot = bot
+        self.db = db
 
+    @commands.command()
+    async def buyhouse(self, ctx):
+        await ctx.send("Rumah berhasil dibeli!")
