0218b12a2a667fe58732aa8fa3437fcb57be4e12
Alexander Raventhorn <arrafiwahyuandreashakim@gmail.com>
2025-04-01 10:29:32 +0700

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,1 +1,10 @@
+class AdminCommands(commands.Cog):
+    def __init__(self, bot, db):
+        self.bot = bot
+        self.db = db
 
+    @commands.command()
+    @commands.is_owner()
+    async def resetuser(self, ctx, user: discord.Member):
+        self.db.reset_user(user.id)
+        await ctx.send(f"User {user.name} berhasil direset!")
