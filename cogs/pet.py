44607b9f8daa80f99b44f904d0eb07ea7f9bf37e
Alexander Raventhorn <arrafiwahyuandreashakim@gmail.com>
2025-04-01 10:23:54 +0700

    Update pet.py

--- cogs/pet.py
+++ cogs/pet.py
@@ -1,1 +1,9 @@
+class PetSystem(commands.Cog):
+    def __init__(self, bot, db):
+        self.bot = bot
+        self.db = db
 
+    @commands.command()
+    async def pet(self, ctx):
+        pet = self.db.get_pet(ctx.author.id)
+        await ctx.send(f"Your pet: {pet['name']} (Level {pet['level']})")
