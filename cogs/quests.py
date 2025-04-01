5dc4482a0cb132506da07341b537a41ebef71ed7
Alexander Raventhorn <arrafiwahyuandreashakim@gmail.com>
2025-04-01 10:30:51 +0700

    Update quests.py

--- cogs/quests.py
+++ cogs/quests.py
@@ -1,1 +1,9 @@
+class QuestSystem(commands.Cog):
+    def __init__(self, bot, db):
+        self.bot = bot
+        self.db = db
 
+    @commands.command()
+    async def quest(self, ctx):
+        quests = self.db.get_quests(ctx.author.id)
+        await ctx.send(f"Active quests: {', '.join(quests)}")
