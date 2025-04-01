8020b32c8d2ad276b837f1d45ee3b63cbea2fd9e
Alexander Raventhorn <arrafiwahyuandreashakim@gmail.com>
2025-04-01 10:25:18 +0700

    Update economy.py

--- cogs/economy.py
+++ cogs/economy.py
@@ -1,1 +1,9 @@
+class EconomySystem(commands.Cog):
+    def __init__(self, bot, db):
+        self.bot = bot
+        self.db = db
 
+    @commands.command()
+    async def balance(self, ctx):
+        user = self.db.get_user(ctx.author.id)
+        await ctx.send(f"Gold: {user['gold']} | Gems: {user['gems']}")
