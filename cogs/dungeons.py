07014d4dc6c75c02b057683114b5ae302f320581
Alexander Raventhorn <arrafiwahyuandreashakim@gmail.com>
2025-04-01 10:34:07 +0700

    Update dungeons.py

--- cogs/dungeons.py
+++ cogs/dungeons.py
@@ -1,1 +1,8 @@
+class DungeonSystem(commands.Cog):
+    def __init__(self, bot, db):
+        self.bot = bot
+        self.db = db
 
+    @commands.command()
+    async def enter_dungeon(self, ctx, name: str):
+        await ctx.send(f"Entering dungeon: {name}")
