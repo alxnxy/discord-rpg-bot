f74aa7d9d14fda8443c590972a321e2644dcc04b
Alexander Raventhorn <arrafiwahyuandreashakim@gmail.com>
2025-04-01 10:32:03 +0700

    Update achievement.py

--- cogs/achievement.py
+++ cogs/achievement.py
@@ -1,1 +1,9 @@
+class AchievementSystem(commands.Cog):
+    def __init__(self, bot, db):
+        self.bot = bot
+        self.db = db
 
+    @commands.command()
+    async def achievements(self, ctx):
+        achievements = self.db.get_achievements(ctx.author.id)
+        await ctx.send(f"Unlocked: {', '.join(achievements)}")
